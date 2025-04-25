69cdeed505934cc19c47128daec284a00d0f19d5
gordongu <1559005+gordongu@users.noreply.github.com>
2025-04-25 00:22:21 -0600

    Assignment 8 initial commit

---
+++ base_external/package/aesdchar/aesdchar.mk
@@ -0,0 +1,16 @@
+AESDCHAR_VERSION = '2b55c6b945a0d8e6807508643af153739d8fa3fc'
+AESDCHAR_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-gordongu.git'
+AESDCHAR_SITE_METHOD = git
+AESDCHAR_GIT_SUBMODULES = YES
+
+# Add aesdchar module and build
+AESDCHAR_MODULE_SUBDIRS += aesd-char-driver
+
+# Add aesdchar load and unload scripts
+define AESDCHAR_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin
+endef
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -1,9 +1,9 @@
-LDD_VERSION = 6071b8d7a3185ae2df97348ac16cffc23809012d
-LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-gordongu.git
+LDD_VERSION = '6071b8d7a3185ae2df97348ac16cffc23809012d'
+LDD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-gordongu.git'
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
 
-LDD_MODULE_SUBDIRS = misc-modules
+LDD_MODULE_SUBDIRS += misc-modules
 LDD_MODULE_SUBDIRS += scull
 
 $(eval $(kernel-module))
